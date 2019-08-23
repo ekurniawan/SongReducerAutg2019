782238b03939ed59ad747bd9e51a8966b1d40eda
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-08-23 14:20:48 +0700

    menambahkan selected song

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {selectSong} from '../actions';
 
 
 class SongList extends React.Component{
+    renderList(){
+        return this.props.songs.map((song)=>{
+            return(
+            <div className="item" key={song.title}>
+                <div className="right floated content">
+                    <button onClick={()=>this.props.selectSong(song)} className="ui button primary">
+                        Select
+                    </button>
+                </div>
+                <div className="content">{song.title}</div>
+            </div>)
+        });
+    }
     render(){
-        console.log(this.props.songs);
-        return(<div>SongList</div>);
+        //console.log(this.props.songs);
+        return(<div className="ui divided list">{this.renderList()}</div>);
     }
 }
 
@@ -13,4 +27,4 @@ const mapStateToProps = (state)=>{
     return{songs:state.songs};
 };
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps,{selectSong})(SongList);
